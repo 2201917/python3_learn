2c459509e5f60df561d7a6cd71a730495ae0c6a9
2201917 <2201917@gmail.com>
2019-01-12 23:31:02 +0300

    новые функции

---
+++ v_stepen.py
@@ -0,0 +1,9 @@
+x=float (input ("Введите Х: "))
+y=float (input ("Введите Y: "))
+a=x**y
+print (x, "в степени",y,"=",a)
+b=x//y
+print ("X разделить на Y=",b)
+c=x%y
+print ("остаток от деления",с)
+print()
---
+++ while.py
@@ -0,0 +1,12 @@
+x=int(input("Введите число"))
+while x>0:
+    y=x
+    while y>0:
+        y-=1
+        print (y)
+    x-=1
+print("Stop")
+for x in 1,5,2,4,3:
+    print (x**2)
+    
+    
